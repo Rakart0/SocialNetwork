d0e06b552bd2c70237fdcb6792cd118462184c63
Michael Harouchi <rakart0@gmail.com>
2019-02-18 16:55:52 +0100

    Integration-Start
    HAHAHA HOHOHO HIHIHI JPP

---
+++ Socialnetwork.Webclient/Controllers/LoginController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Socialnetwork.Webclient.Areas.Identity.Pages.Account;
+using Socialnetwork.Webclient.Models;
+using SocialNetwork.Data.Models;
+using SocialNetwork.Data.Models.IdentityModels;
+using SocialNetwork.Data.Repository.Interfaces;
+
+namespace Socialnetwork.Webclient.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly IRepoUser repo;
+
+        public LoginController(
+            SignInManager<ApplicationUser> _signInManager, 
+            UserManager<ApplicationUser> _userManager,
+            IRepoUser _repo)
+        {
+            userManager = _userManager;
+            signInManager = _signInManager;
+            repo = _repo;
+        }
+
+        public IActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                //return acceuil
+
+            }
+            else
+            {
+                return View("signin");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RegisterAsync (AuthViewModel authViewModel)
+        {
+
+            
+            if (!ModelState.IsValid)
+            {
+                return View("signin",authViewModel);
+
+            }
+
+            var userToRegister = new ApplicationUser
+            {
+                UserName = authViewModel.UserName,
+                Email = authViewModel.Email
+            };
+
+            var result =await userManager.CreateAsync(userToRegister, authViewModel.Password);
+
+            if (result.Succeeded)
+            {
+                var u = new User { UserName = userToRegister.UserName, ApplicationUser = userToRegister, Email = userToRegister.Email, UserPictureUrl = "www.google.be" };
+                repo.AddUser(u);
+                //_logger.LogInformation("User created a new account with password.");
+
+                //var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                //var callbackUrl = Url.Page(
+                //    "/Account/ConfirmEmail",
+                //    pageHandler: null,
+                //    values: new { userId = user.Id, code = code },
+                //    protocol: Request.Scheme);
+
+                //await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                //    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+                await signInManager.SignInAsync(userToRegister, isPersistent: false);
+                return LocalRedirect("Index");
+            }
+
+
+            return View();
+        }
+
+    }
+}
